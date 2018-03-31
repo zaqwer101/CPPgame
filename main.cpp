01e2d3e922658679b5d1f4d0d45a8dd90e2bcceb
zaqwer101 <platonov256@gmail.com>
2018-03-31 10:07:49 +0000

    РНемного ÐNetbeans

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
-#include <iostream>
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: zaqwer
+ *
+ * Created on March 31, 2018, 10:06 AM
+ */
+
+#include <cstdlib>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
     return 0;
 }
+
