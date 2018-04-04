b7ef73e414f76cc9b4a316b47dfddca4f24fb010
zaqwer101 <platonov256@gmail.com>
2018-04-04 03:32:30 +0000

    Описал конструктор класса Creature

--- Creature.cpp
+++ Creature.cpp
@@ -1,6 +1,20 @@
 #include "Creature.h"
 
-Creature::Creature() {
+Creature::Creature(string name, int hp, int mana, int armor, int damage) 
+{
+    this->stats.armor = armor;
+    this->stats.maxarmor = armor;
+    this->stats.name = name;
+    this->stats.hp = hp;
+    this->stats.maxhp = hp;
+    this->stats.mana = mana;
+    this->stats.maxmana = mana;
+    this->stats.damage = damage;
+    this->stats.maxdamage = damage;
+    
+    this->stats.level = 1;
+    this->stats.exp = 0;
+    this->stats.exp_to_level = 100;
 }
 
 
